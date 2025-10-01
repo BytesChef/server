f6bf62cc1a04a1d94f91d638d5bf4a7d073903f9
lai2-24 <lai2-24@itismeucci.com>
2025-10-01 10:00:01 +0200

    aggiunta tubi

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,9 @@
 package com.example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -20,5 +23,7 @@ public final class App {
         ServerSocket ss = new ServerSocket(3500);
         Socket ht = ss.accept();
         System.out.println(("qualcuno si è collegato"));
+        bufferedReader in = new BufferedReader(new InputStreamReader(ht.getInputStream()));
+        PrintWriter out = new PrintWriter(ht.getOutputStream(), true);  
     }
 }
